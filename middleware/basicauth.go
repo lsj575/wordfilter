4b88537c3f25eab9e69d2096a80954d87c49558c
sijie.long <768712778@qq.com>
2019-02-28 09:03:39 +0800

    fix:basicauth

--- middleware/basicauth.go
+++ middleware/basicauth.go
@@ -7,6 +7,6 @@ import "github.com/kataras/iris/middleware/basicauth"
 // BasicAuth middleware sample.
 var BasicAuth = basicauth.New(basicauth.Config{
 	Users: map[string]string{
-		"root": "token123",
+		"root": "root",
 	},
 })
